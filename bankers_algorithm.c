1fed3077b91ce5f78c52484ba47fbc866477f01d
Abhishek Vishwakarma <abhishek.k.v76076@gmail.com>
2018-02-25 21:17:09 +0530

    test case added

--- bankers_algorithm.c
+++ bankers_algorithm.c
@@ -3,6 +3,37 @@
 #include <stdio.h>
 #include <pthread.h>
 
+/* Sample Test Case
+
+   no. of processes = 5
+   no. of resources = 3
+
+   available resources = 3 3 2
+
+   allocation matrix
+   [ 0 1 0
+     2 0 0
+     3 0 2
+     2 1 1
+     0 0 2 ]
+
+   maximum required resources matrix
+   [ 7 5 3
+     3 2 2
+     9 0 2
+     2 2 2
+     4 3 3 ]
+
+
+   need matrix (to calculate) {max - alloc}
+   [ 7 4 3
+     1 2 2
+     6 0 0
+     0 1 1
+     4 3 1 ]
+
+*/
+
 int main(int argc, char** argv) {
-	// to implement
+	// input parsing	
 }
