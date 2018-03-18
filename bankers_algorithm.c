3d3d4e702d1fdfa73d97b12d815a4c40d0aa9d77
Abhishek Vishwakarma <abhishek.k.v76076@gmail.com>
2018-03-18 20:05:10 +0530

    safe seq code and thread code added in main

--- bankers_algorithm.c
+++ bankers_algorithm.c
@@ -2,7 +2,9 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <pthread.h>
+#include <stdbool.h>
 
 int nResources,
     nProcesses;
@@ -12,8 +14,13 @@ int **maxRequired;
 int **need;
 int *safeSeq;
 
-void getSafeSeq() {
+bool getSafeSeq() {
 	// get safe sequence - use safeSeq[]
+	return true;
+}
+
+void* processCode(void* arg) {
+	// thread code
 }
 
 int main(int argc, char** argv) {
@@ -63,8 +70,33 @@ int main(int argc, char** argv) {
                         need[i][j] = maxRequired[i][j] - allocated[i][j];
 
 	// get safe sequence
+	safeSeq = (int *)malloc(nProcesses * sizeof(*safeSeq));
+        for(int i=0; i<nProcesses; i++) safeSeq[i] = -1;
+
+        if(!getSafeSeq()) {
+                printf("\nUnsafe State! The processes leads the system to a unsafe state.\n\n");
+                exit(-1);
+        }
+
+        printf("\n\nSafe Sequence Found : ");
+        for(int i=0; i<nProcesses; i++) {
+                printf("%-3d", safeSeq[i]+1);
+        }
+        printf("\nExecuting Processes...\n\n");
+        sleep(1);
 	
 	// run threads
+	pthread_t processes[nProcesses];
+        pthread_attr_t attr;
+        pthread_attr_init(&attr);
+
+        for(int i=0; i<nProcesses; i++)
+                pthread_create(&processes[i], &attr, processCode, (void *)i);
+
+        for(int i=0; i<nProcesses; i++)
+                pthread_join(processes[i], NULL);
+
+        printf("\nAll Processes Finished\n");	
 	
 	// free resources
         free(resources);
